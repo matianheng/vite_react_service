ae5478bc4517895cf285b862fd0e256ff312a96d
matianheng <1157838984@qq.com>
2024-02-26 13:46:55 +0800

    vite_React 创建

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import  path  from "path"; // 引入方法
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()
+   
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src')
+    },
+  },
+  server: {
+    port:2023,  //端口号
+    host:true,  //可以设置固定的值或者是布尔值, 设置后,才会有NetWork
+    open:true,  //运行自动打开浏览器
+    // 配置反向代理，解决跨域
+    proxy: {
+ 
+      '/middle_platform': {
+        target: 'http://mpl.qa.sgmlink.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/middle_platform/, '')
+      }
+    },
+ 
+  },
+})
